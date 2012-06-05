6c510a30f8c9d1079976ec6fc8f63d8ac969dbb3
Scott Brenner <scott@scottbrenner.com>
2012-06-05 18:04:57 -0400

    Added ViewController Classes for a tab view.

---
+++ MyCourt Dates/MCDTabEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  MCDTabEventViewController.h
+//  MyCourt Dates
+//
+//  Created by Scott Brenner on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MCDTabEventViewController : UIViewController
+
+@end
